f0469404c42461e27f7030745e805f5729db4d15
Luan338 <luan38.eric@gmail.com>
2021-05-11 00:56:57 -0300

    Primeiro desafio JavaScript

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Meu primeiro desafio: trabalhando com variaveis");
+
+const nome = "Luan";
+let idade = 21;
+let anoAtual = 2021;
+
+const anoDeNascimento = anoAtual - idade;
+
+let souHumano = true;
+let numero1 = 12;
+let numero2 = 10;
+let operador = numero1 + numero2;
+console.log(`Oi, meu nome é ${nome} e nasci no ano de ${anoDeNascimento}. Eu sei que a soma entre ${numero1} e ${numero2} é ${operador}`);
